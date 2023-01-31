dfe7d5df21aae0c79aa03855819f0ffe450083cf
Arun Kumar <112628113+Akgoldie@users.noreply.github.com>
2023-01-31 23:31:13 +0530

    Delete BURGERS.py

--- BURGERS.py
+++
@@ -1,9 +0,0 @@
-# cook your dish here
-n=int(input())      #n--> limit for no of times (for loop)
-for i in range(n):
-    patties,buns=map(int,input().split(" "))    #get values (patties,buns)
-    #print(patties,buns)
-    if(buns<=patties):
-        print(buns)
-    else:
-        print(patties)
